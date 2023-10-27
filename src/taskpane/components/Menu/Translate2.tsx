1585967cca04a2c44ace195edbb5b300ab9eee19
Tim Bicker <timbicker2010@gmail.com>
2023-10-27 19:41:22 +0200

    update Translate

--- src/taskpane/components/Menu/Translate2.tsx
+++ src/taskpane/components/Menu/Translate2.tsx
@@ -6,34 +6,87 @@ import LanguageSelect from "../LanguageSelect"
 import {AddSprachklausel} from "../AddSprachKlausel"
 import React from "react"
 
-async function processWordDocument() {
-  await Word.run(async context => {
-    const paragraphs = context.document.body.paragraphs
-    paragraphs.load("text")
+async function createTableFromSelection(context: Word.RequestContext, selection: Word.Range) {
+  selection.load("paragraphs/text")
+  await context.sync()
+
+  const nonEmptyParagraphs = selection.paragraphs.items.filter(p => p.text.trim() !== "")
+
+  if (nonEmptyParagraphs.length > 0) {
+    // Create a table with the required number of rows and 2 columns within the selected range
+    const table = selection.insertTable(nonEmptyParagraphs.length, 2, Word.InsertLocation.after, [])
+    table.load("rows/cells")
 
     await context.sync()
 
-    if (paragraphs.items.length > 0) {
-      // Create a table with the required number of rows and 2 columns
-      const table = context.document.body.insertTable(paragraphs.items.length, 2, Word.InsertLocation.end, [])
+    // Iterate through the non-empty paragraphs and set each cell's value
+    for (let i = 0; i < nonEmptyParagraphs.length; i++) {
+      const paraText = nonEmptyParagraphs[i].text
 
-      table.load("rows/cells")
-      await context.sync()
+      const cell1 = table.rows.items[i].cells.items[0]
+      const cell2 = table.rows.items[i].cells.items[1]
 
-      // Iterate through the paragraphs and set each cell's value to the paragraph text
-      for (let i = 0; i < paragraphs.items.length; i++) {
-        const paraText = paragraphs.items[i].text
+      cell1.body.insertParagraph(paraText, Word.InsertLocation.end)
+      cell2.body.insertParagraph(paraText, Word.InsertLocation.end)
+    }
 
-        const cell1 = table.rows.items[i].cells.items[0]
-        const cell2 = table.rows.items[i].cells.items[1]
+    // Delete the original selection after inserting the table
+    selection.delete()
 
-        cell1.body.insertParagraph(paraText, Word.InsertLocation.end)
-        cell2.body.insertParagraph(paraText, Word.InsertLocation.end)
-      }
+    await context.sync()
+  } else {
+    console.log("No non-empty paragraphs found in the selection.")
+  }
+}
 
-      await context.sync()
+async function updateRightCell(context: Word.RequestContext, selection: Word.Range) {
+  const table = selection.parentTable
+  table.load("rows/columns/items")
+  await context.sync()
+
+  // Check if table has two columns
+  const columnsCount = table.rows.items[0].cells.items.length
+  if (columnsCount !== 2) {
+    throw new Error("The table does not have two columns.")
+  }
+
+  // Determine which cell the selection is in
+  const cell = selection.getRange(Word.RangeLocation.whole).parentTableCellOrNullObject
+  cell.load("cellIndex, parentRow")
+  await context.sync()
+
+  if (cell.isNullObject) {
+    throw new Error("Could not determine the cell.")
+  }
+
+  if (cell.cellIndex === 1) {
+    throw new Error("The selection is in the right cell.")
+  }
+
+  const rightCell = cell.parentRow.cells.items[cell.cellIndex + 1]
+  const content = cell.body.getRange("Whole")
+  content.load("text")
+  await context.sync()
+
+  // delete content of right cell
+  rightCell.body.clear()
+  rightCell.body.insertParagraph(content.text, Word.InsertLocation.end)
+
+  await context.sync()
+}
+// setError: (msg: string) => void
+async function processWordDocument() {
+  await Word.run(async context => {
+    // Get the selected range
+    const selection = context.document.getSelection()
+    // Load parentTable property to determine if the selection is inside a table
+    selection.load("parentTable")
+    await context.sync()
+
+    if (!selection.parentTable) {
+      await createTableFromSelection(context, selection)
     } else {
-      console.log("No paragraphs found in the document.")
+      await updateRightCell(context, selection)
     }
   })
 }
