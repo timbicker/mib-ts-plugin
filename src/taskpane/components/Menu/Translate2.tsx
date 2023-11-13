4ea9e5bbe406285ee80d39e139c11729b12ee534
Tim Bicker <timbicker2010@gmail.com>
2023-11-13 19:00:32 +0100

    update table editing

--- src/taskpane/components/Menu/Translate2.tsx
+++ src/taskpane/components/Menu/Translate2.tsx
@@ -6,29 +6,6 @@ import LanguageSelect from "../LanguageSelect"
 import {AddSprachklausel} from "../AddSprachKlausel"
 import React from "react"
 
-class Paragraph {
-  paragraph: Word.Paragraph
-  list: Word.List | undefined
-  constructor(paragraph: Word.Paragraph, list?: Word.List) {
-    this.paragraph = paragraph
-    this.list = list
-  }
-
-  getText(): string {
-    return this.paragraph.text.trim()
-  }
-
-  createNew(body: Word.Body, text: string) {
-    const insertedText = body.insertText(text, Word.InsertLocation.end)
-    insertedText.font.set({
-      bold: this.paragraph.font.bold,
-      italic: this.paragraph.font.italic,
-      name: this.paragraph.font.name,
-      size: this.paragraph.font.size,
-    })
-  }
-}
-
 class ListManager {
   originalList: Word.List | undefined = undefined
   newList: Word.List | undefined = undefined
@@ -76,23 +53,20 @@ class ListManager {
   }
 }
 
-function createNewParagraph(body: Word.Body, oldParagraph: Word.Paragraph) {
-  const text = oldParagraph.text.trim()
-  const newParagraph = body.insertParagraph("", Word.InsertLocation.start)
+function updateParagraph(newParagraph: Word.Paragraph, originalParagraph: Word.Paragraph, text: string) {
+  newParagraph.insertText(text, "Replace")
   newParagraph.font.set({
-    bold: oldParagraph.font.bold,
-    italic: oldParagraph.font.italic,
-    name: oldParagraph.font.name,
-    size: oldParagraph.font.size,
+    bold: originalParagraph.font.bold,
+    italic: originalParagraph.font.italic,
+    name: originalParagraph.font.name,
+    size: originalParagraph.font.size,
   })
-  return newParagraph
 }
 
 async function createTableFromSelection(context: Word.RequestContext, selection: Word.Range) {
   selection.load("paragraphs")
   await context.sync()
-
-  selection.paragraphs.load("font,style,text,listOrNullObject/id")
+  selection.paragraphs.load("font,style,text,listOrNullObject/id,listOrNullObject/levelTypes")
   await context.sync()
 
   const nonEmptyParagraphs = selection.paragraphs.items.filter(p => p.text.trim() !== "")
@@ -103,22 +77,22 @@ async function createTableFromSelection(context: Word.RequestContext, selection:
   if (nonEmptyParagraphs.length > 0) {
     // Create a table with the required number of rows and 2 columns within the selected range
     const table = selection.insertTable(nonEmptyParagraphs.length, 2, Word.InsertLocation.after, [])
-    table.load("rows/cells")
-
+    table.load("rows/cells/body/paragraphs/items")
     await context.sync()
 
     const rightListManager = new ListManager(context)
     const leftListManager = new ListManager(context)
     // Iterate through the non-empty paragraphs and set each cell's value
     for (let i = 0; i < nonEmptyParagraphs.length; i++) {
-      const cell1 = table.rows.items[i].cells.items[0]
-      const cell2 = table.rows.items[i].cells.items[1]
+      const cellLeft = table.rows.items[i].cells.items[0]
+      const cellRight = table.rows.items[i].cells.items[1]
       const originalParagraph = nonEmptyParagraphs[i]
-
-      const leftParagraph = createNewParagraph(cell1.body, originalParagraph)
-      const rightParagraph = createNewParagraph(cell2.body, originalParagraph)
-      // await leftListManager.updateLists(originalParagraph, leftParagraph)
-      // await rightListManager.updateLists(originalParagraph, rightParagraph)
+      const paragraphLeft = cellLeft.body.paragraphs.getFirst()
+      const paragraphRight = cellRight.body.paragraphs.getFirst()
+      updateParagraph(paragraphLeft, originalParagraph, originalParagraph.text.trim())
+      updateParagraph(paragraphRight, originalParagraph, originalParagraph.text.trim())
+      await leftListManager.updateLists(originalParagraph, paragraphLeft)
+      await rightListManager.updateLists(originalParagraph, paragraphRight)
     }
 
     await context.sync()
@@ -185,50 +159,10 @@ async function processStandard(context: Word.RequestContext) {
   }
 }
 
-async function processMinimal(context: Word.RequestContext) {
-  // Get the selected range
-  const selection = context.document.getSelection()
-  // Load parentTable property to determine if the selection is inside a table
-  // selection.load("paragraphs/items/listOrNullObject/id")
-  // await context.sync()
-  selection.load("paragraphs")
-  await context.sync()
-  selection.paragraphs.load("font,style,text,listOrNullObject/id,listOrNullObject/levelTypes")
-  await context.sync()
-
-  const table = selection.insertTable(selection.paragraphs.items.length, 2, Word.InsertLocation.after, [])
-  table.load("rows/cells/body/paragraphs/items")
-  await context.sync()
-  console.log("path synced")
-
-  const rightListManager = new ListManager(context)
-  const leftListManager = new ListManager(context)
-
-  const paragraphs = selection.paragraphs.items
-  for (let i = 0; i < paragraphs.length; i++) {
-    const p = paragraphs[i]
-    const cellLeft = table.rows.items[i].cells.items[0]
-    const paragraphLeft = cellLeft.body.paragraphs.getFirst()
-    paragraphLeft.insertText("Hi", "Replace")
-    const cellRight = table.rows.items[i].cells.items[1]
-    // const leftParagraph = createNewParagraph(cellLeft.body, p)
-    const rightParagraph = createNewParagraph(cellRight.body, p)
-
-    // await leftListManager.updateLists(p, leftParagraph)
-    // await rightListManager.updateLists(p, rightParagraph)
-
-    console.log(`text: ${p.text}`)
-    if (!p.listOrNullObject.isNullObject) {
-      console.log(`id: ${p.listOrNullObject.id}`)
-    }
-  }
-  await context.sync()
-}
-
 async function processWordDocument() {
   await Word.run(async context => {
     try {
-      await processMinimal(context)
+      await processStandard(context)
     } catch (e) {
       console.log(`error: ${e}`)
     }
