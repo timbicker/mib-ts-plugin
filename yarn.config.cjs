e85301751d095a11d2ee61a331d7c555096f1f27
Tim Bicker <timbicker2010@gmail.com>
2024-02-28 21:07:29 +0100

    update yarn constraints

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -3,14 +3,16 @@ const { defineConfig } = require('@yarnpkg/types');
 
 module.exports = defineConfig({
   async constraints({Yarn}) {
+    function checkDependency(name, version) {
+      for (const dep of Yarn.dependencies({ ident: name })) {
+        dep.update(version);
+      }
+    }
+
     for (const workspace of Yarn.workspaces()) {
       workspace.set('engines.node', `18`);
     }
-    for (const dep of Yarn.dependencies({ ident: 'react' })) {
-      dep.update(`18`);
-    }
-    for (const dep of Yarn.dependencies({ ident: 'typescript' })) {
-      dep.update(`5`);
-    }
+    checkDependency('react', '^18')
+    checkDependency('typescript', '^5')
   },
 });
